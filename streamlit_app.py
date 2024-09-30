1be01b3076b4d7d1db6564c4c93b5cad94d5bcd3
Subhin Hwang <107633586+Surihub@users.noreply.github.com>
2024-09-30 10:04:47 +0000

    글자 하나만 수정(대문페이지)

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title("👋🏻 Streamlit 앱 만들기")
 st.subheader("ATC 9월 특강 실습 페이지")
 
 st.success("초록색 창")
-st.link_button("ATC 홈페이지", "https://hicomputing.org:45939/")# st.error("빨간색 창")
+st.link_button("ATC 홈페이지!", "https://hicomputing.org:45939/")# st.error("빨간색 창")
 # st.info("파란색 창")
 # st.warning("노란색 창") # ctrl+/ : 주석처리
 st.image("https://media.giphy.com/media/8bE0EERrvXkq5S9BCa/giphy.gif?cid=ecf05e479tvvde0o3d3do3g67yoq0jn4zzyg9hyt63stuf0x&ep=v1_gifs_related&rid=giphy.gif&ct=g", caption="Welcome to ATC coding") 
