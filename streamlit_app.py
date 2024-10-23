5367e45683442c2446defd59825abfd1e562d865
yoonseo0211 <kongah7@gmail.com>
2024-10-23 10:13:12 +0000

    앱 시작

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
-st.title("👋🏻 Streamlit 앱 만들기")
-st.subheader("이 페이지는 실습 페이지입니다.")
-st.write("오른쪽 위의 'fork' 버튼을 눌러주세요. 이 페이지와 앱이 그대로 복사됩니다.")
+st.title("👋🏻 윤서의 페이지입니다")
+st.subheader("윤서를 소개합니다.")
+st.write("아래 버튼을 눌러보아요.")
 
-st.link_button("streamlit 매뉴얼 페이지 바로가기!", "https://surish.notion.site/streamlit-113eef51495c8083986cc65f2d07470c?pvs=73")
+st.link_button("소개 바로가기!", "https://www.naver.com/")
 
 # st.success("초록색 창")
 # st.error("빨간색 창")
 st.info("파란색 창")
 # st.warning("노란색 창") # ctrl+/ : 주석처리
-st.image("https://media.giphy.com/media/8bE0EERrvXkq5S9BCa/giphy.gif?cid=ecf05e479tvvde0o3d3do3g67yoq0jn4zzyg9hyt63stuf0x&ep=v1_gifs_related&rid=giphy.gif&ct=g", caption="Welcome to coding world") 
+
+st.image("https://media.giphy.com/media/iCh8iLOteYr0Q/giphy.gif?cid=ecf05e47ea6yhl7rvq347icv5e2rs6svqnq52llas0n84aix&ep=v1_gifs_related&rid=giphy.gif&ct=g", caption="윤서")
